821f2445f1be74182908925495d81f7ba7e377bd
r8bywork <vanya.smolensky@gmail.com>
2023-06-02 22:11:53 +0300

    done with workers

--- Controller/workerController.js
+++ Controller/workerController.js
@@ -1,70 +1,7 @@
 import User from "../Models/User.js";
 import Worker from "../Models/Worker.js";
 
-// export const createWorker = async (req, res, next) => {
-// 	try {
-// 		const { id } = req.params;
-// 		const { username, password, roles } = req.body.worker[0];
-// 		console.log(username, password, roles);
-// 		// console.log(req.body);
-// 		const user = await User.findById(id)
-// 			.populate("house")
-// 			.populate("dairy")
-// 			.populate("feedAndAdditives");
-
-// 		if (!user) {
-// 			return res.status(404).json({ error: "User not found" });
-// 		}
-
-// 		const workerDocId = user.workers;
-// 		if (!workerDocId) {
-// 			// If the worker document doesn't exist, create a new document and add the worker details
-// 			const newWorkerDoc = new Worker({
-// 				workers: [
-// 					{
-// 						username,
-// 						password,
-// 						roles,
-// 					},
-// 				],
-// 				house: user.house,
-// 				dairy: user.dairy,
-// 				feedAndAdditives: user.feedAndAdditives,
-// 			});
-
-// 			// Save the new worker document
-// 			const savedWorkerDoc = await newWorkerDoc.save();
-// 			console.log("asdasdasdasdasdasdasasdasdasd");
-// 			// Update the workers field in the user document with the new worker document's ID
-// 			user.workers = savedWorkerDoc._id;
-// 		} else {
-// 			// If the worker document exists, retrieve the document and add the new worker details
-// 			const workerDoc = await Worker.findById(workerDocId);
-
-// 			if (!workerDoc) {
-// 				return res.status(404).json({ error: "Worker document not found" });
-// 			}
-
-// 			workerDoc.worker.push({
-// 				username,
-// 				password,
-// 				roles,
-// 			});
-
-// 			// Save the updated worker document
-// 			await workerDoc.save();
-// 		}
-
-// 		// Save the user document
-// 		await user.save();
-
-// 		res.status(200).json({ message: "Worker created successfully" });
-// 	} catch (error) {
-// 		console.error("Error creating worker:", error);
-// 		res.status(500).json({ error: "Failed to create worker" });
-// 	}
-// };
-
+//good
 export const createWorker = async (req, res, next) => {
 	try {
 		const { id } = req.params;
@@ -131,22 +68,70 @@ export const createWorker = async (req, res, next) => {
 	}
 };
 
-// Функция изменения пользователя по ID
 export const updateUser = async (req, res, next) => {
 	try {
-		const { id } = req.params;
-		const updatedData = req.body; // Обновленные данные пользователя
+		const { userId, workerId } = req.body;
+		// Находим пользователя по его идентификатору
+		const user = await User.findById(userId).populate("workers");
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
 
-		const user = await User.findByIdAndUpdate(id, updatedData, { new: true });
+		// Получаем фактический массив рабочих пользователей
+		const workersArray = user.workers.workers;
+
+		// Находим рабочего пользователя с указанным идентификатором в массиве
+		const workerIndex = workersArray.findIndex(
+			(worker) => worker._id.toString() === workerId
+		);
+		console.log(user.workers.workers[workerIndex]);
+
+		if (workerIndex === -1) {
+			return res.status(404).json({ error: "Worker not found" });
+		}
+
+		// Обновляем данные рабочего пользователя
+		user.workers.workers[workerIndex] = {
+			...user.workers[workerIndex],
+			...req.body.workers[0],
+		};
+
+		await user.workers.save();
+
+		res.status(200).json({ message: "Worker updated successfully" });
+	} catch (error) {
+		console.error("Error updating worker:", error);
+		res.status(500).json({ error: "Failed to update worker" });
+	}
+};
+
+export const getWorkerById = async (req, res, next) => {
+	try {
+		const { userId, workerId } = req.body;
+
+		// Находим пользователя по его идентификатору
+		const user = await User.findById(userId).populate("workers");
 
 		if (!user) {
 			return res.status(404).json({ error: "User not found" });
 		}
 
-		res.status(200).json({ message: "User updated successfully", user });
+		// Получаем фактический массив рабочих пользователей
+		const workersArray = user.workers.workers;
+
+		// Находим рабочего пользователя с указанным идентификатором в массиве
+		const worker = workersArray.find(
+			(worker) => worker._id.toString() === workerId
+		);
+
+		if (!worker) {
+			return res.status(404).json({ error: "Worker not found" });
+		}
+
+		res.status(200).json({ worker });
 	} catch (error) {
-		console.error("Error updating user:", error);
-		res.status(500).json({ error: "Failed to update user" });
+		console.error("Error retrieving worker:", error);
+		res.status(500).json({ error: "Failed to retrieve worker" });
 	}
 };
 
@@ -206,5 +191,71 @@ export const deleteUser = async (req, res, next) => {
 	} catch (error) {
 		console.error("Error deleting worker:", error);
 		res.status(500).json({ error: "Failed to delete worker" });
+	}
+};
+
+//good
+export const deleteRole = async (req, res, next) => {
+	const { userId, workerId, roleIndex } = req.body;
+
+	try {
+		// Find the user by ID
+		const user = await User.findById(userId).populate("workers");
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		const worker = user.workers.workers.find(
+			// (worker) => worker._id.toString() === workerId
+			(w) => w._id.toString() === workerId
+		);
+
+		// Find the worker within the user's workers array
+		// const worker = user.workers.find((w) => w._id.toString() === workerId);
+
+		if (!worker) {
+			return res.status(404).json({ error: "Worker not found" });
+		}
+
+		// Remove the role at the specified index from the worker's roles array
+		const deletedRole = worker.roles.splice(roleIndex, 1)[0];
+		// Save the updated user
+		await user.workers.save();
+
+		res.status(200).json({ deletedRole });
+	} catch (err) {
+		console.error("Error deleting role for worker:", err);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
+
+export const addRole = async (req, res, next) => {
+	const { userId, workerId, roles } = req.body;
+
+	try {
+		// Find the user by ID
+		const user = await User.findById(userId).populate("workers");
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		// Find the worker within the user's workers array
+		const worker = user.workers.workers.find(
+			(w) => w._id.toString() === workerId
+		);
+		if (!worker) {
+			return res.status(404).json({ error: "Worker not found" });
+		}
+
+		// Add roles to the worker's roles array
+		worker.roles.push(...roles.split(","));
+
+		// Save the updated user
+		await user.workers.save();
+
+		res.status(200).json({ message: "Roles added successfully" });
+	} catch (err) {
+		console.error("Error adding roles for worker:", err);
+		res.status(500).json({ error: "Internal server error" });
 	}
 };
--- Routes/workerRouter.js
+++ Routes/workerRouter.js
@@ -1,16 +1,22 @@
 import { Router } from "express";
 import {
+	addRole,
 	createWorker,
+	deleteRole,
 	deleteUser,
 	getUser,
+	getWorkerById,
 	updateUser,
 } from "../Controller/workerController.js";
 
 const router = new Router();
 
-router.put("/update/:id", updateUser);
+router.put("/update", updateUser);
 router.get("/get/:id", getUser);
+router.get("/getOne", getWorkerById);
 router.delete("/delete", deleteUser);
 router.post("/:id", createWorker);
+router.delete("/deleteRole", deleteRole);
+router.put("/addRole", addRole);
 
 export default router;
